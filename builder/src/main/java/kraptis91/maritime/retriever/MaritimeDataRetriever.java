325977abf4df7fd800004ad0107a8c3a007b03aa
Konstantinos Raptis <kraptis@unipi.gr>
2020-12-01 19:48:48 +0200

    builder: -maritime data retriever api updated. model: -vessel pojo updated.

--- builder/src/main/java/kraptis91/maritime/retriever/MaritimeDataRetriever.java
+++ builder/src/main/java/kraptis91/maritime/retriever/MaritimeDataRetriever.java
@@ -6,11 +6,18 @@ import kraptis91.maritime.model.VesselTrajectoryPoint;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 /** @author Konstantinos Raptis [kraptis at unipi.gr] on 1/12/2020. */
 public interface MaritimeDataRetriever {
 
-  VesselTrajectoryPoint getVesselPosition(String vesselName);
+  /**
+   * Get vessel Map by time.
+   *
+   * @param timestamp timestamp in UNIX epochs
+   * @return The vessel Map, which has vessel name as key and current position as value
+   */
+  Map<String, VesselTrajectoryPoint> getVesselPositionMap(BigInteger timestamp);
 
   VesselTrajectoryPoint getVesselPosition(String vesselName, BigInteger timestamp);
 
@@ -19,6 +26,14 @@ public interface MaritimeDataRetriever {
   VesselTrajectory getVesselTrajectory(String vesselName);
 
   List<Vessel> getVesselsByTrajectoryPoint(double longitude, double latitude);
+
+  /**
+   * Get vessels by speed.
+   *
+   * @param speed The speed in knots (allowed values: 0-102.2 knots)
+   * @return The vessel list
+   */
+  List<Vessel> getVesselsBySpeed(double speed);
 
   /**
    * Get vessels by draught specifications.
--- model/src/main/java/kraptis91/maritime/model/Vessel.java
+++ model/src/main/java/kraptis91/maritime/model/Vessel.java
@@ -11,4 +11,66 @@ public class Vessel {
 
   /** Allowed values: 0.1-25.5 meters */
   private double draught;
+
+  private VesselTrajectory vesselTrajectory;
+
+  private Vessel(Builder builder) {
+    this.imo = builder.imo;
+    this.vesselName = builder.vesselName;
+    this.draught = builder.draught;
+    this.vesselTrajectory = builder.vesselTrajectory;
+  }
+
+  public int getImo() {
+    return imo;
+  }
+
+  public String getVesselName() {
+    return vesselName;
+  }
+
+  public double getDraught() {
+    return draught;
+  }
+
+  public void setDraught(double draught) {
+    this.draught = draught;
+  }
+
+  public VesselTrajectory getVesselTrajectory() {
+    if (vesselTrajectory == null) {
+      vesselTrajectory = new VesselTrajectory();
+    }
+    return vesselTrajectory;
+  }
+
+  public static class Builder {
+
+    // mandatory fields
+    private int imo;
+    private String vesselName;
+
+    // non mandatory fields
+    private double draught;
+    private VesselTrajectory vesselTrajectory;
+
+    public Builder(int imo, String vesselName) {
+      this.imo = imo;
+      this.vesselName = vesselName;
+    }
+
+    public Builder withDraught(double draught) {
+      this.draught = draught;
+      return Builder.this;
+    }
+
+    public Builder withVesselTrajectory(VesselTrajectory vesselTrajectory) {
+      this.vesselTrajectory = vesselTrajectory;
+      return Builder.this;
+    }
+
+    public Vessel build() {
+      return new Vessel(Builder.this);
+    }
+  }
 }
