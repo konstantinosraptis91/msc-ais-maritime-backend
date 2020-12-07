5436b93d93364e44a6cac71a542f715ef382f7cc
Konstantinos <kraptis@unipi.gr>
2020-12-07 22:38:06 +0200

    api: -rename Server to Application. -Javalin code inside Application has been remastered.

---
+++ api/src/main/java/kraptis91/maritime/api/Application.java
@@ -0,0 +1,15 @@
+package kraptis91.maritime.api;
+
+import io.javalin.Javalin;
+import kraptis91.maritime.api.controller.VesselController;
+import kraptis91.maritime.api.enums.ServerConfig;
+
+/** @author Konstantinos Raptis [kraptis at unipi.gr] on 7/12/2020. */
+public class Application {
+
+  public static void main(String[] args) {
+    Javalin app = Javalin.create().start(ServerConfig.INSTANCE.getPort());
+    app.get("/", ctx -> ctx.result("Server Is Up and Running..."));
+    app.get("/vessels/:shiptype", VesselController.getVesselsByShipType);
+  }
+}
