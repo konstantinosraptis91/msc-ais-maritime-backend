797c58978e76154ecf8c1e1ca27302897087b535
Konstantinos <kraptis@unipi.gr>
2020-12-11 13:58:33 +0200

    parser module added

--- parser/build.gradle.kts
+++ parser/build.gradle.kts
@@ -8,8 +8,5 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":model"))
-    implementation(project(":parser"))
     testImplementation("junit:junit:4.13")
-    implementation("org.mongodb:mongo-java-driver:3.12.7")
 }
---
+++ parser/src/main/java/kraptis91/maritime/parser/CSVParser.java
@@ -0,0 +1,7 @@
+package kraptis91.maritime.parser;
+
+/**
+ * @author Konstantinos Raptis [kraptis at unipi.gr] on 9/12/2020.
+ */
+public class CSVParser {
+}
