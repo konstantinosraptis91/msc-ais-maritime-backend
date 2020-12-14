9bbddb670909daedba34c3b7947b51e409c8f72e
Konstantinos <kraptis@unipi.gr>
2020-12-14 02:08:49 +0200

    optimize imports

--- db/src/test/java/kraptis91/maritime/dao/MongoVesselTrajectoryPointDaoTest.java
+++ db/src/test/java/kraptis91/maritime/dao/MongoVesselTrajectoryPointDaoTest.java
@@ -1,10 +1,7 @@
 package kraptis91.maritime.dao;
 
-import kraptis91.maritime.parser.utils.DirectoryUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 /** @author Konstantinos Raptis [kraptis at unipi.gr] on 14/12/2020. */
