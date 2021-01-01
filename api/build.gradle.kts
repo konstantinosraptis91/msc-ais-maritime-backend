d3042cc97e196c6e0308288aed86adae51a23a62
Konstantinos <kraptis@unipi.gr>
2021-01-01 15:55:10 +0200

    Fixed jackson annotations warning

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation("io.javalin:javalin:3.12.0")
     implementation("org.slf4j:slf4j-simple:1.7.30")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.0")
 }
 
 application {
